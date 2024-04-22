de6c84e682ec5ea5f6e4996ac508a96c0a35971a
HuzaimaKhan058 <huzaimakhan058@gmail.com>
2024-04-22 23:27:01 +0500

    Cli based multiplication table.

---
+++ index.ts
@@ -0,0 +1,39 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+let answer = await inquirer.prompt(
+    [
+        {
+            name: "tableOf",
+            type: "number",
+            message: "Print table of:"
+        },
+        {
+            name: "tableFrom",
+            type: "number",
+            message:  "From:"
+        },
+        {
+            name: "tableTo",
+            type: "number",
+            message: "Print table to"
+        }
+    ]
+);
+
+if(answer.tableFrom <= answer.tableTo){
+
+    for (let num = answer.tableFrom; num <= answer.tableTo; num++) {
+        let table = answer.tableOf;
+        console.log(`${table} x ${num} = ${table * num}`);
+    }
+};
+
+if(answer.tableFrom >= answer.tableTo){
+
+    for (let num = answer.tableFrom; num >= answer.tableTo; num--) {
+        let table = answer.tableOf;
+        console.log(`${table} x ${num} = ${table * num}`);
+    }
+};
